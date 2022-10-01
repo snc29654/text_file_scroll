bad5a6817b3dfa94ddf8be4ef4f2f9d83df16fda
snc29654 <snc29654l@nifty.com>
2022-10-02 08:16:49 +0900

    スクロール速度を変更可能とする

--- thread_scroll_text.py
+++ thread_scroll_text.py
@@ -37,9 +37,11 @@ from chardet import detect
 class main_class():  
     def __init__(self, main):
         print ("__init__")  
+        self.interval=0.5
+
         button1= Button(root, text=u'終了', command=self.quit)  
         button1.grid(row=0, column=1)  
-        button1.place(x=300, y=30) 
+        button1.place(x=600, y=30) 
         self.textExample=ScrolledText(root, height=30,width=120, wrap=tkinter.CHAR)
         self.textExample.pack()
         self.textExample.place(x=90, y=70)
@@ -52,6 +54,13 @@ class main_class():
         button4.grid(row=0, column=1)  
         button4.place(x=250, y=30) 
 
+        button5= Button(root, text=u'遅く', command=self.button5_clicked)  
+        button5.grid(row=0, column=1)  
+        button5.place(x=350, y=30) 
+
+        button6= Button(root, text=u'早く', command=self.button6_clicked)  
+        button6.grid(row=0, column=1)  
+        button6.place(x=400, y=30) 
 
     def method0(self,message):  
         print ("method0")  
@@ -84,7 +93,7 @@ class main_class():
 
                         print(line, end='')
                         self.textExample.insert(tkinter.END,str(line)+"\n")
-                        time.sleep(0.5)
+                        time.sleep(self.interval)
                         self.textExample.yview_moveto(1)
 
     def button3_clicked(self):  
@@ -103,7 +112,12 @@ class main_class():
     def button4_clicked(self):
         self.kill = 1  
 
+    def button5_clicked(self):
+        self.interval=self.interval + 0.1  
 
+    def button6_clicked(self):
+        if( self.interval > 0.1):
+            self.interval=self.interval - 0.1  
 
 
     def quit(self):
